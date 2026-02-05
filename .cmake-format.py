650d30401280be590db21ec991188b2797acf9bf
Samuel <samueldanielyan@gmail.com>
2026-02-05 16:45:34 +0100

    first basic implementations (not working)

---
+++ .cmake-format.py
@@ -0,0 +1,9 @@
+with section("format"):
+    line_width = 100
+    tab_size = 4
+    use_tabchars = False
+    max_subgroups_hwrap = 2
+    max_lines_hwrap = 2
+
+with section("parse"):
+    additional_commands = {}
